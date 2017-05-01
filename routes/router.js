d4922e6b958655e0995fdabda3a911aab450618e
Alex Acker <alex.acker08@gmail.com>
2017-05-01 20:43:07 +1000

    More files and pages added

--- routes/router.js
+++ routes/router.js
@@ -7,13 +7,36 @@ router.get('/', (req,res,next) => {
 	res.render('index')
 });
 router.get('/about', (req,res,next) => {
-	res.render('about')
+	res.locals.page = 'about';
+	res.render('about');
 });
 router.get('/project', (req,res,next) => {
-	res.render('project')
+	res.locals.page = 'project';
+	res.render('projects')
 });
 router.get('/contact', (req,res,next) => {
 	res.render('contact')
 });
+router.get('/projects/quote'), (req,res,next) => {
+	res.render('project-quote')
+
+}
+router.get('/projects/pagination'), (req,res,next) => {
+	res.render('project-pagination')
+	
+}
+router.get('/projects/bootstrap-form'), (req,res,next) => {
+	res.render('project-bootstrap-form')
+	
+}
+router.get('/projects/tic-tac-toe'), (req,res,next) => {
+	res.locals.projects = 'tictactoe';
+	res.render('project-tic-tac-toe')
+	
+}
+router.get('/projects/movie-search-app'), (req,res,next) => {
+	res.render('project-movie-search-app')
+	
+}
 
 module.exports = router;
