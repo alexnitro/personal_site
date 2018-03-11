35a12dcbe872d155e186fef090e224c430d9a0ad
Alex Acker <alex.acker@gonitro.com>
2018-03-11 14:47:58 +1100

    Adjusting package json rules for startup with heroku

--- routes/router.js
+++ routes/router.js
@@ -6,6 +6,7 @@ router.get('/about', (req,res,next) => {res.render('about');});
 router.get('/project', (req,res,next) => {res.render('projects')});
 router.get('/contact', (req,res,next) => {res.render('contact')});
 router.get('/services', (req,res,next) => {res.render('index')})
+
 router.post('/contact', (req,res) => {
 	console.log(req.body);
 	res.send('Contact has been made with the backend');
